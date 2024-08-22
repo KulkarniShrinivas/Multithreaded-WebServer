3bbfcf523a30dc26b96078a7fce13b667ab287fe
Shrinivas Kulkarni <91586785+KulkarniShrinivas@users.noreply.github.com>
2024-08-23 00:48:56 +0530

    updated

--- SingleThreaded/Client.java
+++ SingleThreaded/Client.java
@@ -1,6 +1,6 @@
-//We are requesting for server so will be creating class for that
-//we will be sending millions or thousands of requests to the server by using Jmeter not by using client
-//will be using Jmeter because in jmeter we can spoun multiple threads and we can send multiple requests to the server
+// requesting for server so will be creating class for that
+//sending millions or thousands of requests to the server by using Jmeter not by using client
+// using Jmeter because in jmeter we can spoun multiple threads and we can send multiple requests to the server
 
 import java.awt.print.Printable;
 import java.io.BufferedReader;
@@ -15,31 +15,14 @@ public class Client {
 
     public void run() throws IOException {
         int port = 8010;
-        //server will be runninning on locol port on machine itself
-        InetAddress adress = InetAddress.getByName("localhost");
-        //locolhost- whatever the machine ip adress is there it will be connected to that
-        //this will be connected to the server//unknown host also can gives exception so we will be makeing it checked
-        //and will be catching as Throw UnknownHostException in run method
+       
 
         Socket socket = new Socket(adress, port);
-        //socket will be throwing IO exception so we will be catching it by IoException
-        //So whatever we have created the socket for client will be having input/output socket
-
-
-
-        //client---->server
-        //toSocket(PrintWriter)------fromSocket(Buffer)  |------------|
-        //PrintWriter(to write)->
-        //Buffer(to read)-> In this whatever bytes we get it will convert into text and give the result
-        //in---------------out
-        //client will be connecting so I will be opening the socket and will be connecting to the server
-        //so I will be using printwriter to write the data to the server
+       
         PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
         BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        //socket input we need to take
-        //so we will be using InputStreamReader to read the data from the socket
-        //so we will be using BufferedReader to read the data from the socket
+       
 
         toSocket.println("Hello from the client");
 
@@ -63,4 +46,4 @@ public class Client {
     }
 
 }
-//output after connecting to the server specific time it wil disconnects and throws an exception
+
--- SingleThreaded/Server.java
+++ SingleThreaded/Server.java
@@ -15,49 +15,26 @@ public class Server{
 
         //we have opened a server socket on this port
         ServerSocket socket = new ServerSocket(port);
-        //ifconfig will give you the ip address of your machine
+        
 
         socket.setSoTimeout(20000);
-        //this will make the server to wait for 10 seconds for the client to connect
-        //if the client does not connect in 10 seconds, the server will throw an exception and it will closes automatically 
-        //but in realtime production socket will be always open
-
-        //this will make the server to wait for the client to connect
+      
         while(true){
 
                  System.out.println("Server is listening on port "+port);
-            //the code will be waiting below line until we get connection from the client
-            //After 10 seconds, the server will throw an exception and it will closes automatically
+           
             Socket acceptedConnection = socket.accept();
-            //this will accept the connection from the client(So whenever will get connection )
-            //this will return the socket object which will be used to communicate with the client
+            
             System.out.println("Connection accepted from client: "+acceptedConnection.getInetAddress());
             //this will print the socket ip address of the client ^
                 PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream(), true);
 
-             //printwriter
-            //this will be used to send the data to the client
-            //From client -> sever will accept the connection and we were listenimng IP and Port(8010)
-            //So when c1 requests the server then server have opened the socket
-            //so client----->server
-            // ------------------- (opened socket)(output stream will be from server and input stream will be from client)
-            //socket open then will be getting bits/bytes
-            //so I need to write something on outputstream so using printwriter class we can write
-
-            //I will be using BufferedReader to read the data from the client
-           //Input stream I will be using buffer reader
-           //so in this whatever bytes will get it will combine  and give the result
-           //PrintWriter->  In this whatever we give text it will convert into bytes and send to the client
-           //BufferedReader-> In this whatever bytes we get it will convert into text and give the result
-
+        
             BufferedReader fromclient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
 
             toClient.println("Hello from the Server");
 
-            //client---->server
-            //fromclient------toclient  |------------|
-            //in---------------out
-
+         
             toClient.close();
             fromclient.close();
             acceptedConnection.close();
@@ -66,8 +43,7 @@ public class Server{
 
 
     }
-    //I will be creating this class because the function which we defined is not static method
-    //if its not static method then it will not be stored in memory or it will not be there
+    
 
     public static void main(String[] args){
         Server server = new Server();
@@ -79,13 +55,6 @@ public class Server{
     }
 
 }
-
-//Single Threaded Conclusion
-//after running 50000 throughputs
-//In single threaded we are doing light weight operation
-//So we are getting 50000 throughputs
-//So we are getting 50000 requests per second
-//If I am reading file or writing file then it will be heavy operation so other threads will be waiting or it will be in queue or deadlock
 
 
 //jmeter apache-jmeter-5.6.3/bin
